aeb0f5d3e60d52707e2b9f2cc08c7dba05c24e88
CurvyLantern <ashfaqnasim33@gmail.com>
2023-03-05 16:27:35 +0600

    removed log from prod

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,8 @@
 import Fastify, { FastifyInstance } from 'fastify';
 
-const app: FastifyInstance = Fastify({ logger: true });
+const app: FastifyInstance = Fastify({
+	logger: {
+		enabled: process.env.NODE_ENV === 'development',
+	},
+});
 export default app;
