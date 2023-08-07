35ac59175c49cef74a2206d15cda6ffb17c040f4
naseem(T_T) <ashfaqnaseem1@gmail.com>
2023-08-07 15:03:56 +0600

    fixing broken server

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,39 @@
-import app from './app';
-import { SAT_NAMES } from '../db/SAT_NAMES';
-import { TLE } from '../db/TLE2';
-import env from '@fastify/env';
-import EnvOptions from './plugins/env';
-import cors from '@fastify/cors';
+import app from "./app";
+import { SAT_NAMES } from "../db/SAT_NAMES";
+import { TLE } from "../db/TLE2";
+import env from "@fastify/env";
+import EnvOptions from "./plugins/env";
+import cors from "@fastify/cors";
 
 const start = async () => {
-	try {
-		await app
-			.register(env, EnvOptions)
-			.register(cors, {
-				origin: '*',
-			})
-			.after();
+  try {
+    await app
+      .register(env, EnvOptions)
+      .register(cors, {
+        origin: "*",
+      })
+      .after();
 
-		app.get('/', (req, res) => {
-			return { hello: 'world' };
-		});
+    app.get("/", (req, res) => {
+      return { hello: "world" };
+    });
 
-		app.get('/v1/tle', async (req, res) => {
-			return TLE;
-		});
-		app.get('/v1/info/satellite', async (req, res) => {
-			return SAT_NAMES;
-		});
+    app.get("/v1/tle", async (req, res) => {
+      return TLE;
+    });
+    app.get("/v1/info/satellite", async (req, res) => {
+      return SAT_NAMES;
+    });
 
-		//@ts-ignore
-		await app.listen({ port: app.config.PORT });
+    //@ts-ignore
+    await app.listen({ port: app.config.PORT });
 
-		const address = app.server.address();
-		const port = typeof address === 'string' ? address : address?.port;
-	} catch (err) {
-		app.log.error(err);
-		process.exit(1);
-	}
+    const address = app.server.address();
+    const port = typeof address === "string" ? address : address?.port;
+    console.log(`server listening at ${JSON.stringify(address)}`);
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
 };
 start();
